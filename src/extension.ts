23a215f6cfd218f3df96191cd23ae5d2ffa21b74
goweii <goweii@163.com>
2023-05-01 21:44:24 +0800

    feat: delete useless files

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
 import * as vscode from 'vscode';
-import { registerAllCommands } from './main/commands';
-import { assetsConfig } from './main/assets_config';
-import { assetsWatcher } from './main/assets_watcher';
-import { pubspecWatcher } from './main/pubspec_watcher';
-import { assetsGenerator } from './main/assets_generator';
+import { registerAllCommands } from './commands';
+import { assetsConfig } from './assets_config';
+import { assetsWatcher } from './assets_watcher';
+import { pubspecWatcher } from './pubspec_watcher';
+import { assetsGenerator } from './assets_generator';
 
 export async function activate(context: vscode.ExtensionContext) {
 	console.log('Extension is activate');
